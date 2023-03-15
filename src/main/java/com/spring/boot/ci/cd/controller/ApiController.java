4d6c041c3758047d43d9d8a589f3ba4a8fa8c0dd
nnallu_nike <nagababu.nalluri@nike.com>
2023-03-15 14:32:11 +0530

    Initial commit

---
+++ src/main/java/com/spring/boot/ci/cd/controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.spring.boot.ci.cd.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+  @GetMapping("api/health")
+  public ResponseEntity<?> healthCheck(){
+    return ResponseEntity.ok("200 response..!");
+  }
+
+}
